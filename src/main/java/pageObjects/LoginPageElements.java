18e7affde080ba6d14fb1aa8f81e7847d9c11c46
German147 <barreragerman27@gmail.com>
2022-10-05 20:29:29 -0300

    Initial commit

---
+++ src/main/java/pageObjects/LoginPageElements.java
@@ -0,0 +1,6 @@
+package main.java.pageObjects;
+
+public interface LoginPageElements {
+    String loginText = "<h1 class=\"center-card__title\">¡Hola, german! Para seguir, ingresá tu contraseña</h1>";
+    String emailAddress = "email-badge__user-name";
+}
---
+++ src/main/java/utils/RetryAnalizer.java
@@ -0,0 +1,19 @@
+package main.java.utils;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalizer implements IRetryAnalyzer {
+
+    int count =0;
+    int retrycount =1;
+
+    @Override
+    public boolean retry(ITestResult iTestResult) {
+        while (count<retrycount){
+            count++;
+            return true;
+        }
+        return false;
+    }
+}
